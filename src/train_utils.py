49f1194f1d170a9859a0e14eb684eb0db0f514e5
Asa Gilmore <asaagilmore@gmail.com>
2024-07-01 10:39:55 -0700

    rework training struct

---
+++ src/train_utils.py
@@ -0,0 +1,31 @@
+import torch
+
+
+def train(model, loader, criterion, optimizer, device):
+    model.train()
+    running_loss = 0.0
+    for images, masks in loader:
+        images, masks = images.to(device), masks.to(device)
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, masks)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+    return running_loss/len(loader)
+
+
+def validate(model, loader, criterion, device):
+    model.eval()
+    val_loss = 0.0
+    correct = 0
+    with torch.no_grad():
+        for inputs, labels in loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            val_loss += loss.item()
+            preds = torch.argmax(outputs, dim=1)
+            correct += (preds == labels).sum().item()
+    return val_loss / len(loader), correct / len(loader.dataset)
