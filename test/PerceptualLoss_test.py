25d2663db81d1253cf7251436641ed7bc5285730
Asa Gilmore <asaagilmore@gmail.com>
2024-07-02 10:58:08 -0700

    fix unit tests

--- test/PerceptualLoss_test.py
+++ test/PerceptualLoss_test.py
@@ -12,7 +12,7 @@ import pytest  # noqa: E402
 def setup_perceptual_loss():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     feature_extractor = VGG16FeatureExtractor().to(device)
-    perceptual_loss = PerceptualLoss(feature_extractor, MSELoss()).to(device)
+    perceptual_loss = PerceptualLoss(feature_extractor, MSELoss())
     return perceptual_loss, device
 
 
@@ -22,7 +22,7 @@ def test_perceptual_loss_equality(setup_perceptual_loss):
     input_tensor = torch.rand(1, 1, 224, 224, device=device)
     target_tensor = input_tensor.clone()
 
-    loss = perceptual_loss(input_tensor, target_tensor)
+    loss = perceptual_loss.get_loss(input_tensor, target_tensor)
     assert_bool = loss.item() == pytest.approx(0, abs=1e-6)
     assert assert_bool, "Loss should be close to " + \
                         "0 for identical inputs and targets."
@@ -34,7 +34,7 @@ def test_perceptual_loss_difference(setup_perceptual_loss):
     input_tensor = torch.rand(1, 1, 224, 224, device=device)
     target_tensor = torch.zeros(1, 1, 224, 224, device=device)
 
-    loss = perceptual_loss(input_tensor, target_tensor)
+    loss = perceptual_loss.get_loss(input_tensor, target_tensor)
 
     assert loss.item() > 0.1, 'Loss should be high for significantly ' + \
                               'different inputs and targets.'
--- test/misc_utils_test.py
+++ test/misc_utils_test.py
@@ -6,9 +6,14 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 
 from misc_utils import get_matched_ids, get_filepath_from_id  # noqa: E402
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
 
 def test_get_matched_ids():
-    dirs = ["./test_matched_ids/MRA", "./test_matched_ids/T1W"]
+    dirs = [
+        os.path.join(current_dir, "./test_matched_ids", "MRA"),
+        os.path.join(current_dir, "./test_matched_ids", "T1W")
+    ]
 
     should_contain = ['IXI055', 'IXI056', 'IXI057', 'IXI058']
     should_not_contain = ['IXI059', 'IXI060']
@@ -21,7 +26,8 @@ def test_get_matched_ids():
 
 
 def test_get_filepath_from_id():
-    dir = "./test_data/MRA"
+    dir = os.path.join(current_dir, "test_data", "MRA")
     id = "IXI055"
     filepath = get_filepath_from_id(dir, id)
-    assert filepath == "./test_data/MRA/IXI055-MRA.nii.gz"
+    assert filepath == os.path.join(current_dir, "test_data", "MRA",
+                                    "IXI055-MRA.nii.gz")
