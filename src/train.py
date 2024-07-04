a63e11661cf230b5a83cf7cc1e5380613d4965f1
Asa Gilmore <asaagilmore@gmail.com>
2024-07-04 11:53:55 -0700

    fix warn about depreciated verbose arg

--- src/train.py
+++ src/train.py
@@ -106,8 +106,7 @@ if __name__ == "__main__":
     # def optimizer
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
-                                                     factor=0.5, patience=10,
-                                                     verbose=True)
+                                                     factor=0.5, patience=10)
     num_epochs = args.num_epochs
 
     # load checkpoint if exists
@@ -134,7 +133,7 @@ if __name__ == "__main__":
         print(f"Epoch {epoch+1}, Loss: {train_loss}, Val Loss: {val_loss}")
         logging.info(f"Epoch {epoch+1}, Loss: {train_loss}, "
                      f"Val Loss: {val_loss}"
-                     f"LR: {optimizer.param_groups[0]['lr']}")
+                     f"LR: {scheduler.get_last_lr()}")
 
         tensorboard_write(writer, device, train_loss, val_loss, epoch+1,
                           model, valid_dataloader,
